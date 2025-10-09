8c1c8dd2869261a6abcbecfc1db7cf2f9fc07936
Francisco Alisson Ribeiro da Costa <thelastofusalisson@gmail.com>
2025-10-09 16:25:19 -0300

    css

--- treino/script/script.js
+++ treino/script/script.js
@@ -1,43 +1,41 @@
 const tarefa = document.getElementById('taskInput');
-const adicionar = document.getElementById('addBtn');
+const botao = document.getElementById('addBtn');
 const lista = document.getElementById('lista-tarefa');
 
-adicionar.addEventListener('click', function() {
-    const addTarefa = tarefa.value.trim();
-    
-    if (addTarefa === '') {
-        alert('Preencha corretamente...');
+botao.addEventListener("click", function() {
+    const tarefaText = tarefa.value.trim();
+
+    if (tarefaText === '') {
+        alert('Preencha os dados corretamente!');
         return;
     }
 
-    // Criando elemento
+    // criando o li
     const li = document.createElement('li');
-    li.textContent = addTarefa;
+    li.textContent = tarefaText;
 
-    // criando o botão de concluir
-    const doneBtn = document.createElement('button');
-    doneBtn.textContent = '✔'
-    doneBtn.addEventListener('click', function() {
+    //botao de concluir
+    const concluirBtn = document.createElement('button');
+    concluirBtn.textContent = 'V';
+    concluirBtn.addEventListener("click", function() {
         li.classList.toggle("completed");
     })
 
-    // Criando o botão de remover
+    //botao de remover
     const removeBtn = document.createElement("button");
-    removeBtn.textContent = '❌';
-    removeBtn.addEventListener('click', function() {
+    removeBtn.textContent = "X";
+    removeBtn.addEventListener("click", function() {
         lista.removeChild(li)
     });
 
-    // Adiciona os botões ao li
-    li.appendChild(doneBtn);
+    // Adiciona os botões aos li
+    li.appendChild(concluirBtn);
     li.appendChild(removeBtn);
 
-    // Adiciona o li a lista
+    // Adiciona li a lista ul
     lista.appendChild(li);
 
-    // Limpa o campo do texto
+    tarefa.value="";
     tarefa.focus();
-    tarefa.value = "";
-
 
 })
